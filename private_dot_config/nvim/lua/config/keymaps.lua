ca15b31af49e1240d59cd8b1b817e9fc3286dd39
Marco <1marco.salvi@gmail.com>
2024-10-18 13:31:30 +0000

    update

--- private_dot_config/nvim/lua/config/keymaps.lua
+++ private_dot_config/nvim/lua/config/keymaps.lua
@@ -65,3 +65,5 @@ vim.keymap.set("v", "p", '"_dP')
 vim.keymap.set("v", "<leader>p", "p")
 
 vim.keymap.set("i", "<C-BS>", "<C-w>", { desc = "Delete word in insert mode" })
+
+vim.keymap.set("n", "ZF", "ZQ", { desc = "Quit without saving" })
