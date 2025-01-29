79e6534f1e38e4254320cc4b79bbbff92c76b03a
Marco Salvi <marco.salvi@ingv.it>
2025-01-29 23:51:16 +0100

    nvim up

--- .config/nvim/lua/plugins/autopairs.lua
+++ .config/nvim/lua/plugins/autopairs.lua
@@ -1,18 +1,8 @@
 return {
 	"windwp/nvim-autopairs",
 	event = "InsertEnter",
-	config = true,
 	dependencies = {
 		"nvim-treesitter/nvim-treesitter",
 	},
-	opts = {
-		disable_filetype = { "TelescopePrompt", "spectre_panel" },
-		enable_check_bracket_line = true,
-		ts_config = {
-			lua = { "string" },
-			go = { "string" },
-			java = false,
-			javascript = { "template_string" },
-		},
-	},
+	opts = {},
 }
--- .config/nvim/lua/plugins/dadbod.lua
+++ .config/nvim/lua/plugins/dadbod.lua
@@ -11,8 +11,8 @@ return {
 		"DBUIFindBuffer",
 	},
 	init = function()
-		-- Your DBUI configuration
 		vim.g.db_ui_use_nerd_fonts = 1
+		vim.g.db_ui_auto_execute_table_helpers = 1
 	end,
 	keys = {
 		{
