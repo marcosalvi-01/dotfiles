d53f9a971302a606a562023ab7313967a421479d
Marco Salvi <marco.salvi@ingv.it>
2025-01-29 10:04:19 +0100

    blink

--- .config/nvim/lua/plugins/blink.cmp.lua
+++ .config/nvim/lua/plugins/blink.cmp.lua
@@ -24,7 +24,10 @@ return {
 		},
 
 		sources = {
-			default = { "lsp", "path", "snippets", "buffer" },
+			default = { "lsp", "path", "snippets", "buffer", "dadbod" },
+			providers = {
+				dadbod = { name = "Dadbod", module = "vim_dadbod_completion.blink" },
+			},
 		},
 		completion = {
 			list = {
