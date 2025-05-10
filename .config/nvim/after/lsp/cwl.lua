26ea9b6a582f918e031f4d52f54a0f4c651db88d
Marco Salvi <marco.salvi@ingv.it>
2025-05-10 17:46:44 +0200

    fix cwl

--- .config/nvim/after/lsp/cwl.lua
+++ .config/nvim/after/lsp/cwl.lua
@@ -1,16 +1,8 @@
-local lspconfig = require("lspconfig")
-local configs = require("lspconfig.configs")
-
--- CWL Language Server
-if not configs.cwl_lsp then
-	configs.cwl_lsp = {
-		default_config = {
-			cmd = { "benten-ls" },
-			root_dir = lspconfig.util.root_pattern(".git"),
-			filetypes = { "cwl" },
-		},
-	}
-end
-lspconfig.cwl_lsp.setup({})
-
-return {}
+return {
+	cmd = { "benten-ls" },
+	root_markers = { ".git" },
+	filetypes = { "cwl" },
+	on_attach = function(client, bufnr)
+		vim.notify("benten-ls attached")
+	end,
+}
--- .config/nvim/lua/config/generic.lua
+++ .config/nvim/lua/config/generic.lua
@@ -10,11 +10,6 @@ vim.diagnostic.config({
 	float = { border = "rounded" },
 })
 
--- Add a new file type .cwl
-vim.filetype.add({ extension = { cwl = "cwl" } })
--- Register cwl files as yaml for syntax highlighting
-vim.treesitter.language.register("yaml", { "yaml", "cwl" })
-
 -- Configure .http files
 vim.filetype.add({ extension = { http = "http" } })
 vim.treesitter.language.register("http", { "http", "http" })
@@ -26,19 +21,6 @@ vim.api.nvim_create_autocmd("BufEnter", {
 	callback = function()
 		vim.diagnostic.enable(false)
 	end,
-})
-
--- Set custom indentation rules for .cwl files
-vim.api.nvim_create_autocmd({ "FileType" }, {
-	desc = "Change indentation rules for .cwl files",
-	group = vim.api.nvim_create_augroup("cwl-indentation", { clear = true }),
-	callback = function()
-		vim.bo.expandtab = true
-		vim.bo.shiftwidth = 2
-		vim.bo.tabstop = 2
-		vim.bo.softtabstop = 2
-	end,
-	pattern = { "cwl" },
 })
 
 -- for hyprland conf
