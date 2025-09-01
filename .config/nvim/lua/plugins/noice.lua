0e67abd577b9f20487e7fab0d2c555b6c8d49ce7
Marco Salvi <marco.salvi@ingv.it>
2025-09-01 14:38:38 +0200

    hide supermaven notifications

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -16,17 +16,21 @@ return {
 				enabled = false,
 			},
 		},
-		-- you can enable a preset for easier configuration
 		presets = {
-			bottom_search = false, -- use a classic bottom cmdline for search
-			command_palette = true, -- position the cmdline and popupmenu together
-			long_message_to_split = true, -- long messages will be sent to a split
-			inc_rename = false, -- enables an input dialog for inc-rename.nvim
-			lsp_doc_border = true, -- add a border to hover docs and signature help
+			bottom_search = false,
+			command_palette = true,
+			long_message_to_split = true,
+			inc_rename = false,
+			lsp_doc_border = true,
 		},
 		commands = {
-			history = {
-				view = "confirm",
+			history = { view = "confirm" },
+		},
+		routes = {
+			-- Hide any notification containing "Supermaven"
+			{
+				filter = { event = "notify", find = "Supermaven" },
+				opts = { skip = true },
 			},
 		},
 	},
