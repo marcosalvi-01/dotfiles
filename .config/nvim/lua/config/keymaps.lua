6b3e3b40eeb268e3eb922542931f1f75d29677bd
Marco Salvi <1marco.salvi@gmail.com>
2025-09-20 15:03:24 +0200

    use wofi

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -266,8 +266,6 @@ vim.keymap.set("n", "<leader>tc", function()
 	vim.cmd("tabc")
 end, { desc = "Close current tab" })
 
-vim.keymap.set("n", "<leader>das", Snacks.dashboard.open, { desc = "Open snacks dashboard" })
-
 vim.keymap.set({ "n", "v" }, "g.", function()
 	vim.cmd("normal! @@")
 end, { desc = "Replay last macro" })
