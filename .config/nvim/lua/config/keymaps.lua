bb99d9969cffea65105dc3dc283e382009904640
Marco Salvi <marco.salvi@ingv.it>
2026-05-18 14:24:55 +0200

    fix quickfixlist neovim

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -192,7 +192,7 @@ local function smart_delete(key)
 	local l = vim.api.nvim_win_get_cursor(0)[1]                -- Get the current cursor line number
 	local line = vim.api.nvim_buf_get_lines(0, l - 1, l, true)[1] -- Get the content of the current line
 	return (line:match("^%s*$") and '"_' or "") ..
-	key                                                        -- If the line is empty or contains only whitespace, use the black hole register
+		key                                                    -- If the line is empty or contains only whitespace, use the black hole register
 end
 local keys = { "d", "x", "c", "s", "C", "X" }                  -- Define a list of keys to apply the smart delete functionality
 -- Set keymaps for both normal and visual modes
@@ -309,10 +309,18 @@ vim.keymap.set("n", "<C-i>", "<C-i>zz")
 
 -- Quickfix navigation (with cycles)
 vim.keymap.set("n", "<C-p>", function()
-	vim.cmd("try | cprevious | catch | clast | catch")
+	if vim.tbl_isempty(vim.fn.getqflist()) then
+		vim.notify("Quickfix list is empty", vim.log.levels.INFO)
+		return
+	end
+	vim.cmd("try | cprevious | catch | clast | catch | endtry")
 	vim.cmd.normal("zz")
 end, { desc = "[G]o to previous [E]ntry in the Quickfix list" })
 vim.keymap.set("n", "<C-n>", function()
-	vim.cmd("try | cnext | catch | cfirst | catch")
+	if vim.tbl_isempty(vim.fn.getqflist()) then
+		vim.notify("Quickfix list is empty", vim.log.levels.INFO)
+		return
+	end
+	vim.cmd("try | cnext | catch | cfirst | endtry")
 	vim.cmd.normal("zz")
 end, { desc = "[G]o to next [E]ntry in the Quickfix list" })
